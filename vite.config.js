d5da4717669096e4f095b7e7463425470bbc4873
Galaxy <tabgalaxya553@gmail.com>
2025-05-14 11:31:43 +0000

    Listen delete vite config and node modules and rewite again

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
+// Get directory name in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -15,7 +16,7 @@ export default defineConfig({
     },
   },
   server: {
-    port: 9002,
-    host: '0.0.0.0',
+    port: 9002, // Default port for your app
+    host: '0.0.0.0', // Allows access from network
   }
 })
